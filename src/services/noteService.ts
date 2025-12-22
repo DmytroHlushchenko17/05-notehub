dc3b0d56d41f324e7f3411c03e71a308378b7dbc
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2025-12-22 22:13:59 +0200

    add get noteservice

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,6 +1,26 @@
-import React from "react";
+import type { Note } from "../types/note";
+import axios from "axios";
 
-export default function fetchNotes() {}
+const API_URL = "https://notehub-public.goit.study/api";
+
+interface NoteResponse {
+  results: Note[];
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
+
+export const fetchNotes = async (
+  query: string,
+  page: number = 1
+): Promise<{
+  results: Note[];
+  total_pages: number;
+}> => {
+  const response = await axios.get<NoteResponse>(`${API_URL}/auth/notes`, {
+    Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+  });
+};
 
 export default function createNote() {}
 
--- src/types/note.ts
+++ src/types/note.ts
@@ -1,1 +1,18 @@
-export interface Note {}
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: string;
+}
+
+export interface NoteCreate {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export interface NoteDelete {
+  id: string;
+}
